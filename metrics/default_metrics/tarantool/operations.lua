372b82cec847806d31f274a25ffd3ce131ed0be2
Pavel Yudin <pavel@tarantool.org>
2020-02-25 11:17:58 +0300

    Move an operation name from the metric name to a tag
    Closes #40

--- metrics/default_metrics/tarantool/operations.lua
+++ metrics/default_metrics/tarantool/operations.lua
@@ -8,8 +8,8 @@ local function update_operations_metrics()
     local current_stat = box.stat()
 
     for k, v in pairs(current_stat) do
-        utils.set_gauge('stats_op_' .. k:lower() .. '_total', 'Total amount of ' .. k:lower() .. 's', v.total)
-        utils.set_gauge('stats_op_' .. k:lower() .. '_rps', 'Total RPS ' .. k:lower() .. 's', v.rps)
+        utils.set_gauge('stats_op_total', 'Total amount of operations', v.total, {operation = k:lower()})
+        utils.set_gauge('stats_op_rps', 'Total RPS', v.rps, {operation = k:lower()})
     end
 end
 
