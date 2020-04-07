5cdeac3618a5d39bf1e6761584a9cb1efe5b8645
Max Melentiev <m.melentiev@corp.mail.ru>
2020-04-07 16:07:33 +0300

    Remove code duplication

--- test/utils.lua
+++ test/utils.lua
@@ -2,24 +2,9 @@ local t = require('luatest')
 
 local utils = {}
 
--- a < b
-local function subset_of(a, b)
-    for name, value in pairs(a) do
-        if b[name] ~= value then
-            return false
-        end
-    end
-    return true
-end
-
--- a = b
-local function equal_sets(a, b)
-    return subset_of(a, b) and subset_of(b, a)
-end
-
 function utils.find_obs(metric_name, label_pairs, observations)
     for _, obs in pairs(observations) do
-        local same_label_pairs = equal_sets(obs.label_pairs, label_pairs)
+        local same_label_pairs = pcall(t.assert_equals, obs.label_pairs, label_pairs)
         if obs.metric_name == metric_name and same_label_pairs then
             return obs
         end
