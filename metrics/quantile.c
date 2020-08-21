e35096a8e254961808c9c4fd7061ee7b9f829a91
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2020-08-21 23:31:37 +0300

    ffi optimization

--- metrics/quantile.c
+++ metrics/quantile.c
@@ -1,12 +1,4 @@
-#include <sys/types.h>
-typedef struct {int Delta, Width; double Value; } sample;
-
 int cmpfunc (const void * a, const void * b) {
-		sample _a = (*(sample*)a ), _b = *(sample*)b;
-        if (_a.Value < _b.Value)
-            return -1;
-        if (_a.Value > _b.Value)
-            return 1;
-
-		return 0;
+		double _a = (*(double*)a ), _b = *(double*)b;
+        return _a - _b;
 }
--- metrics/quantile.lua
+++ metrics/quantile.lua
@@ -1,6 +1,7 @@
 local fiber = require('fiber')
 local ffi = require('ffi')
 
+local quantile = {}
 ffi.cdef[[
 	typedef struct {int Delta, Width; double Value; } sample;
 	void qsort(void *base, size_t nitems, size_t size, int (*compar)(const void *, const void*));
@@ -10,23 +11,20 @@ local y = ffi.load('metrics/libquantile.so')
 
 local sample = ffi.metatype('sample', {})
 
-local SAMPLE_SIZE = ffi.sizeof(sample(0,0,0))
+local DOUBLE_SIZE = ffi.sizeof('double')
 
-local function len(x) return ffi.sizeof(x) / SAMPLE_SIZE
-end
-
-local quantile = {}
-
-local function sort_samples(samples)
-	ffi.C.qsort(samples, ffi.sizeof(samples)/SAMPLE_SIZE, SAMPLE_SIZE, y.cmpfunc)
+local function sort_samples(samples, len)
+	ffi.C.qsort(samples, len, DOUBLE_SIZE, y.cmpfunc)
 end
 
 local function make_sample(value, width, delta)
-	return sample(delta, width, value)
+	return sample(delta or 0, width or 0, value)
 end
 
-local function new_array_sample(len)
-	return ffi.cast('sample*', ffi.gc(ffi.C.malloc(SAMPLE_SIZE*len), ffi.C.free))
+local function insert_sample(sample, value, width, delta)
+	sample.Value = value
+	sample.Width = width
+	sample.Delta = delta
 end
 
 local stream = {}
@@ -34,15 +32,15 @@ local stream = {}
 -- Stream computes quantiles for a stream of float64s.
 function stream.new(f, max_samples)
     if not max_samples then
-        max_samples = 500
+        max_samples = 200
     end
 	return setmetatable({
         stream = {
 			f = f,
-			l = ffi.new('sample[?]', 0),
+			l = {},
 			n = 0,
         },
-		b = ffi.new('sample[?]', 0),
+        b = {},
         sorted = true,
         __max_samples = max_samples,
     }, { __index = stream })
@@ -50,79 +48,75 @@ end
 
 function stream:flush()
 	self:maybeSort()
-	self:merge(self.b)
-	self.b = ffi.new('sample[?]', 0)
+	self:merge(self.b, self.b_len)
+	self.b_len = 0
 end
 
 function stream:maybeSort()
 	if not self.sorted then
 		self.sorted = true
-		sort_samples(self.b)
+		sort_samples(self.b, self.__max_samples)
     end
 end
 
 function stream:flushed()
-    return len(self.stream.l) > 0
+    return self.stream.l_len > 0
 end
 
-local function sample_insert(arr, val, pos)
-	local len = ffi.sizeof(arr) / SAMPLE_SIZE
+local function sample_copy(dst, src)
+	dst.Value = src.Value
+	dst.Width = src.Width or 0
+	dst.Delta = src.Delta or 0
+end
+
+local ins_cnt = 0
+local function sample_insert(arr, value, width, delta, len, pos)
+	local do_shift = true
 	if not pos then
-		pos = len
+		pos = len + 1
+		do_shift = false
 	end
-	local new_arr = ffi.new('sample[?]', len + 1)
-	for i = 0, pos - 1 do
-		new_arr[i] = arr[i]
+	if do_shift  then
+		for i = len + 1, pos + 1, -1 do
+			sample_copy(arr[i], arr[i-1])
+		end
 	end
-	new_arr[pos] = val
-	for i = pos + 1, len do
-		new_arr[i] = arr[i-1]
-	end
-	return new_arr
+    insert_sample(arr[pos], value, width, delta )
 end
 
-local function sample_remove(arr, pos)
-	local len = ffi.sizeof(arr) / SAMPLE_SIZE
-	if not pos then
-		error('Required argement pos is missing')
+local function sample_remove(arr, len, pos)
+	for i = pos, len - 1 do
+		sample_copy(arr[i], arr[i+1])
 	end
-
-	local new_arr = ffi.new('sample[?]', len - 1)
-
-	for i = 0, pos do
-		new_arr[i] = arr[i]
-	end
-	for i = pos + 1, len - 1 do
-		new_arr[i - 1] = arr[i]
-	end
-	return new_arr
-
+	arr[len].Value = math.huge
 end
 
-function stream:merge(samples)
+function stream:merge(samples, len)
 	local s = self.stream
 
 	local i = 1
-	local r = 0
-
-    for z = 0, len(samples)-1 do
-        if i % 500 == 0 then
-			fiber.yield()
-        end
-		for j = i, len(s.l) - 1 do
-	        local c = s.l[j]
-			if c.Value > samples[z].Value then
-				s.l = sample_insert(s.l, make_sample(samples[z].Value, samples[z].Width, s.f(s, r) - 1), j)
+    local r = 0
+    for z = 1, len do
+        if i % 1000 == 0 then
+            fiber.yield()
+		end
+		local sample = samples[z-1]
+		for j = i, s.l_len do
+            local c = s.l[j]
+			if c.Value > sample then
+                sample_insert(s.l, sample, 1, s.f(s, r) - 1, s.l_len, j)
+				s.l_len = s.l_len + 1
 
 				i = j + 1
 				goto inserted
             end
 			r = r + c.Width
 		end
-		s.l = sample_insert(s.l, make_sample(samples[z].Value, samples[z].Width, 0))
+		sample_insert(s.l, sample, 1, 0, s.l_len)
+		s.l_len = s.l_len + 1
 		i = i + 1
 	::inserted::
-		s.n = s.n + samples[z].Width
+		s.n = s.n + 1
 	end
 end
 
@@ -131,14 +125,14 @@ function stream:query(q)
     local t = q * s.n
 	t = t + s.f(s, t) / 2
 
-    local p = make_sample(-math.huge, 0, 0)
+    local p = s.l[0]
 	local r = 0
-	for i = 0, len(self.stream.l) - 1  do
-	    if i % 500 == 0 then
+	for i = 1, s.l_len do
+        if i % 500 == 0 then
             fiber.yield()
         end
-		local c = self.stream.l[i]
-		if r + c.Width + c.Delta > t then
+        local c = s.l[i]
+        if r + c.Width + c.Delta > t then
             return p.Value
 		end
 		r = r + p.Width
@@ -148,24 +142,27 @@ function stream:query(q)
 end
 
 function stream:compress()
-    local s = self.stream
-    if len(s.l) < 2 then
+	local s = self.stream
+    if s.l_len < 2 then
 		return
 	end
-	local x = s.l[len (s.l)]
-	local xi = len(s.l)
+	local x = make_sample(0)
+	sample_copy(x, s.l[s.l_len])
+	local xi = s.l_len
 	local r = s.n - x.Width
 
-    for i = len(s.l) - 2, 0, -1 do
-	    if i % 500 == 0 then
+    for i = s.l_len - 1, 1, -1 do
+        if i % 1000 == 0 then
             fiber.yield()
-        end
-		local c = s.l[i]
+		end
+		local c = make_sample(0)
+		sample_copy(c, s.l[i])
 		if c.Width + x.Width + x.Delta <= s.f(s, r) then
 			x.Width = x.Width + c.Width
-			s.l[xi] = x
+            sample_copy(s.l[xi], x)
+			sample_remove(s.l, s.l_len, i)
+			s.l_len = s.l_len - 1
 
-			s.l = sample_remove(s.l, i)
 			xi = xi - 1
 		else
 			x = c
@@ -175,11 +172,20 @@ function stream:compress()
 	end
 end
 
-function quantile.NewTargeted(quantiles)
+function quantile.NewTargeted(quantiles, max_samples)
+	local qs={}
+	local epss = {}
+	for q, eps in pairs(quantiles) do
+		table.insert(qs, q)
+		table.insert(epss, eps)
+	end
+	local len = #qs
 	local function f(s, r)
         local m = math.huge
         local f
-		for q, eps in pairs(quantiles) do
+		for i = 1, len do
+			local q = qs[i]
+			local eps = epss[i]
 			if q*s.n <= r then
 				f = (2 * eps * r) / q
 			else
@@ -191,16 +197,30 @@ function quantile.NewTargeted(quantiles)
 		end
 		return math.max(m, 1)
 	end
-	return stream.new(f)
-end
+	local s = stream.new(f, max_samples)
+	local inf_obj = make_sample(math.huge)
+	local minf_obj = make_sample(-math.huge)
+	s.b = ffi.new('double[?]', s.__max_samples)
 
+	for i = 0, s.__max_samples - 1 do
+        s.b[i] = math.huge
+	end
+	s.stream.l = ffi.new('sample[?]', s.__max_samples * 2 + 2)
+	s.stream.l[0] = minf_obj
+	for i = 1, s.__max_samples * 2 + 1 do
+        s.stream.l[i] = inf_obj
+	end
+	s.b_len = 0
+	s.stream.l_len = 0
+	return s
+end
 
 -- Insert inserts v into the stream.
 function quantile.Insert(stream, v)
-	local sample = make_sample(v, 1, 0)
-	stream.b = sample_insert(stream.b, sample)
+	stream.b[stream.b_len] = v
+	stream.b_len = stream.b_len + 1
 	stream.sorted = false
-	if len(stream.b) == stream.__max_samples then
+	if stream.b_len == stream.__max_samples then
 		stream:flush()
 		stream:compress()
     end
@@ -213,29 +233,24 @@ function quantile.Query(stream, q)
 	if not stream:flushed() then
 		-- Fast path when there hasn't been enough data for a flush;
 		-- this also yields better accuracy for small sets of data.
-		local l = len(stream.b)
+		local l = stream.b_len
 		if l == 0 then
 			return 0
         end
 		local i = math.modf(l * q + 1)
 		stream:maybeSort()
-		return stream.b[i].Value
+		return stream.b[i]
 	end
 	stream:flush()
 	return stream:query(q)
 end
 
--- Merge merges samples into the underlying streams samples.
-function quantile.Merge(stream, samples)
-	sort_samples(samples)
-	stream:merge(samples)
-end
 
 -- Reset reinitializes and clears the list reusing the samples buffer memory.
 function quantile.Reset(stream)
-    stream.stream.l = ffi.new('sample[?]', 0)
     stream.stream.n = 0
-	stream.b = ffi.new('sample[?]', 0)
+	stream.b_len = 0
+	stream.stream.l_len = 0
 end
 
 return quantile
