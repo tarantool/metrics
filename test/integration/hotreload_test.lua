80f211ae4674307a3d398e16a59c2bb545726877
Michael Filonenko <filonenko.mikhail@gmail.com>
2021-05-13 14:27:19 +0300

    Simplife hotreload test

--- test/integration/hotreload_test.lua
+++ test/integration/hotreload_test.lua
@@ -8,35 +8,15 @@ g.test_reload = function()
     box.cfg{}
 
     local metrics = require('metrics')
-    -- create a counter
-    local http_requests_total_counter = metrics.counter('http_requests_total')
 
+    local http_requests_total_counter = metrics.counter('http_requests_total')
     http_requests_total_counter:inc(1, {method = 'GET'})
 
-    local cpu_usage_gauge = metrics.gauge('cpu_usage', 'CPU usage')
-
-    metrics.register_callback(function()
-        local current_cpu_usage = 42
-        cpu_usage_gauge:set(current_cpu_usage, {app = 'tarantool'})
-    end)
-
-    -- create a histogram
-    local http_requests_latency_hist = metrics.histogram(
-        'http_requests_latency', 'HTTP requests total', {2, 4, 6})
-
-    -- somewhere in the HTTP requests middleware:
-    local latency = math.random(1, 10)
-    http_requests_latency_hist:observe(latency)
-
-    -- create a summary
     local http_requests_latency = metrics.summary(
         'http_requests_latency', 'HTTP requests total',
         {[0.5]=0.01, [0.9]=0.01, [0.99]=0.01}
     )
-
-    -- somewhere in the HTTP requests middleware:
-    local latency = math.random(1, 10)
-    http_requests_latency:observe(latency)
+    http_requests_latency:observe(10)
 
     require('metrics.default_metrics.tarantool').enable()
     require('metrics.tarantool.luajit').enable()
@@ -50,4 +30,5 @@ g.test_reload = function()
     require('metrics')
     
     t.assert_not_equals(require('metrics.default_metrics.tarantool').enable(), nil, "Metrics can't reload")
+   require('metrics.tarantool.luajit').enable()
 end
