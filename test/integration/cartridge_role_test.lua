d823f253c8fd810ce2c7e4c76026557eff797e6a
Michael Filonenko <filonenko.mikhail@gmail.com>
2021-05-14 15:32:39 +0300

    Revert "Fix lint"
    This reverts commit 40f2c193030228a874eb5d43c1b2c38d13949f2f.

--- test/integration/cartridge_role_test.lua
+++ test/integration/cartridge_role_test.lua
@@ -1,7 +1,9 @@
 local fio = require('fio')
 local t = require('luatest')
+local yaml = require('yaml')
 local g = t.group()
 
+local utils = require('test.utils')
 local helpers = require('test.helper')
 
 local function set_export(export)
