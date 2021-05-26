428ac3039529b6227927d5d0631581513dc0a24f
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-05-26 17:38:36 +0300

    Fix error message in test

--- test/collectors/histogram_test.lua
+++ test/collectors/histogram_test.lua
@@ -4,5 +4,5 @@ local g = t.group()
 local metrics = require('metrics')
 
 g.test_unsorted_buckets_error = function()
-    t.assert_error_msg_contains('Invalid value for objectives', metrics.histogram, 'latency', nil, {0.9, 0.5})
+    t.assert_error_msg_contains('Invalid value for buckets', metrics.histogram, 'latency', nil, {0.9, 0.5})
 end
