64de095dab7f0372b2d0e4fc740b3b9cfa14f67d
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-07-06 17:57:19 +0300

    skip if version less 2.3.0

--- test/integration/cartridge_hotreload_test.lua
+++ test/integration/cartridge_hotreload_test.lua
@@ -1,11 +1,15 @@
 local fio = require('fio')
 local t = require('luatest')
 local g = t.group()
+local utils = require('test.utils')
 
 local helpers = require('test.helper')
 
-g.before_each( function()
+g.before_each(function()
     t.skip_if(type(helpers) ~= 'table', 'Skip cartridge test')
+    local cartridge_version = require('cartridge.VERSION')
+    t.skip_if(cartridge_version == 'unknown')
+    t.skip_if(utils.is_version_less(cartridge_version, '2.3.0'))
     g.cluster = helpers.Cluster:new({
         datadir = fio.tempdir(),
         server_command = helpers.entrypoint('srv_with_hotreload'),
@@ -21,15 +25,12 @@ g.before_each( function()
         },
     })
     g.cluster:start()
-    local main_server = g.cluster:server('main')
-    t.skip_if(main_server.net_box:eval([[ return pcall(require, 'cartridge.roles') == false ]]))
-    t.skip_if(main_server.net_box:eval([[ return require('cartridge.roles').reload == nil ]]))
-end )
+end)
 
-g.after_each( function()
+g.after_each(function()
     g.cluster:stop()
     fio.rmtree(g.cluster.datadir)
-end )
+end)
 
 local function upload_config()
     local main_server = g.cluster:server('main')
