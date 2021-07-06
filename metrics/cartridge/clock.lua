0953427b16b8eccff2080682768d88de62b824ed
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-07-06 14:17:14 +0300

    Fix linter

--- metrics/cartridge/clock.lua
+++ metrics/cartridge/clock.lua
@@ -1,8 +1,8 @@
 local utils = require('metrics.utils')
 local membership = require('membership')
 
+-- from https://github.com/tarantool/cartridge/blob/cc607f5a6508449608f3953a3f93669e8c8c4ab0/cartridge/issues.lua#L375
 local function update_clock_metrics()
-    -- from https://github.com/tarantool/cartridge/blob/cc607f5a6508449608f3953a3f93669e8c8c4ab0/cartridge/issues.lua#L375
 
     local min_delta = 0
     local max_delta = 0
