cc861aed59e7fdb66c497dbf4ff628bb330967d8
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-07-06 17:12:14 +0300

    fix cfg param

--- test/entrypoint/srv_basic.lua
+++ test/entrypoint/srv_basic.lua
@@ -5,6 +5,9 @@ require('strict').on()
 local log = require('log')
 local errors = require('errors')
 local cartridge = require('cartridge')
+local utils = require('test.utils')
+local cartridge_version = require('cartridge.VERSION')
+
 errors.set_deprecation_handler(function(err)
     log.error('%s', err)
     os.exit(1)
@@ -14,7 +17,8 @@ local ok, err = errors.pcall('CartridgeCfgError', cartridge.cfg, {
     roles = {
         'cartridge.roles.metrics',
     },
-    roles_reload_allowed = true,
+    roles_reload_allowed =
+        (cartridge_version == 'unknown' or utils.is_version_less(cartridge_version, '2.4.0')) and nil or true,
 })
 if not ok then
     log.error('%s', err)
