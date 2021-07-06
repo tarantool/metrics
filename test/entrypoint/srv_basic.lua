98290aea8bf79c2873aea7b6d5d96add70fd5d53
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2021-07-06 17:18:55 +0300

    fix cfg param again

--- test/entrypoint/srv_basic.lua
+++ test/entrypoint/srv_basic.lua
@@ -13,12 +13,17 @@ errors.set_deprecation_handler(function(err)
     os.exit(1)
 end)
 
+local roles_reload_allowed
+
+if cartridge_version ~= 'unknown' and utils.is_version_less(cartridge_version, '2.4.0') then
+    roles_reload_allowed = true
+end
+
 local ok, err = errors.pcall('CartridgeCfgError', cartridge.cfg, {
     roles = {
         'cartridge.roles.metrics',
     },
-    roles_reload_allowed =
-        (cartridge_version == 'unknown' or utils.is_version_less(cartridge_version, '2.4.0')) and nil or true,
+    roles_reload_allowed = roles_reload_allowed,
 })
 if not ok then
     log.error('%s', err)
