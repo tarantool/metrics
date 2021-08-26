fa39a053273496bced86c22f82183573e935eb19
Igor Zolotarev <63460867+yngvar-antonsson@users.noreply.github.com>
2021-08-26 13:55:33 +0300

    Refactor metrics.utils and change type of luajit metrics (#291)

--- metrics/utils.lua
+++ metrics/utils.lua
@@ -2,14 +2,19 @@ local metrics = require('metrics')
 
 local TNT_PREFIX = 'tnt_'
 
-local function prefix_name(name)
-    return TNT_PREFIX .. name
-end
-
-local function set_gauge(name, description, value, labels)
-    local gauge = metrics.gauge(prefix_name(name), description)
+local function set_gauge(name, description, value, labels, prefix)
+    prefix = prefix or TNT_PREFIX
+    local gauge = metrics.gauge(prefix .. name, description)
     gauge:set(value, labels or {})
     return gauge
+end
+
+local function set_counter(name, description, value, labels, prefix)
+    prefix = prefix or TNT_PREFIX
+    local counter = metrics.counter(prefix .. name, description)
+    counter:reset(labels or {})
+    counter:inc(value, labels or {})
+    return counter
 end
 
 local function box_is_configured()
@@ -18,6 +23,6 @@ end
 
 return {
     set_gauge = set_gauge,
+    set_counter = set_counter,
     box_is_configured = box_is_configured,
-    prefix_name = prefix_name,
 }
