e6a76fe37f7553fbf33399d7d3e91b7e31b54b0f
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2022-01-18 01:43:43 +0300

    Remove debul logging

--- test/tarantool/vinyl_test.lua
+++ test/tarantool/vinyl_test.lua
@@ -26,6 +26,5 @@ g.test_vinyl_metrics_present = function()
     local metrics_cnt = fun.iter(metrics.collect()):filter(function(x)
         return x.metric_name:find('tnt_vinyl')
     end):length()
-    require'log'.error(metrics.collect())
     t.assert_equals(metrics_cnt, 20)
 end
