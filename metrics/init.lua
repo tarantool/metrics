591e4d6fc33084c9667e6383ec6f0dc9945d0ca8
Igor Zolotarev <i.zolotarev@corp.mail.ru>
2022-01-21 16:28:05 +0300

    Log deprecation message

--- metrics/init.lua
+++ metrics/init.lua
@@ -120,7 +120,8 @@ return {
     set_global_labels = set_global_labels,
     enable_default_metrics = function(include, exclude)
         log.warn('metrics tnt_net_sent_rps, tnt_net_received_rps, tnt_net_connections_rps, '..
-            'tnt_net_requests_rps, tnt_stats_op_rps ' ..
+            'tnt_net_requests_rps, tnt_stats_op_rps, tnt_space_count, tnt_fiber_count, ' ..
+            'lj_gc_total, tnt_cpu_count, tnt_cpu_total ' ..
             'are deprecated and will be removed in next releases.')
         require('metrics.tarantool').enable(include, exclude)
     end,
