e22df57b7babe537b5b5d6d10cdea5a5997284a3
Andrei Sidorov <andrei.sidorov@vk.team>
2022-07-22 15:08:28 +0300

    rename new metrics > net_per_thread_

--- metrics/tarantool/network.lua
+++ metrics/tarantool/network.lua
@@ -11,19 +11,13 @@ local function update_network_metrics()
 
     collectors_list.net_sent_total =
         utils.set_counter('net_sent_total', 'Totally sent in bytes', box_stat_net.SENT.total)
-    collectors_list.net_sent_rps =
-        utils.set_gauge('net_sent_rps', 'Sending RPS', box_stat_net.SENT.rps)
     collectors_list.net_received_total =
         utils.set_counter('net_received_total', 'Totally received in bytes', box_stat_net.RECEIVED.total)
-    collectors_list.net_received_rps =
-        utils.set_gauge('net_received_rps', 'Receive RPS', box_stat_net.RECEIVED.rps)
 
     -- https://github.com/tarantool/doc/issues/760
 
     -- before tnt version 2.2.0
     if box_stat_net.CONNECTIONS ~= nil and type(box_stat_net.CONNECTIONS) ~= 'number' then
-        collectors_list.net_connections_rps =
-            utils.set_gauge('net_connections_rps', 'Connection RPS', box_stat_net.CONNECTIONS.rps)
         collectors_list.net_connections_total =
             utils.set_counter('net_connections_total', 'Connections total amount', box_stat_net.CONNECTIONS.total)
         collectors_list.net_connections_current =
@@ -34,8 +28,6 @@ local function update_network_metrics()
     end
 
     if box_stat_net.REQUESTS ~= nil then
-        collectors_list.net_requests_rps =
-            utils.set_gauge('net_requests_rps', 'Requests RPS', box_stat_net.REQUESTS.rps)
         collectors_list.net_requests_total =
             utils.set_counter('net_requests_total', 'Requests total amount', box_stat_net.REQUESTS.total)
         collectors_list.net_requests_current =
@@ -64,75 +56,61 @@ local function update_network_metrics()
     if box.stat.net.thread ~= nil then
         local box_stat_net_per_thread = box.stat.net.thread()
 
-        for index, per_thread_mertics in pairs(box_stat_net_per_thread) do
-            collectors_list.net_sent_total =
-                utils.set_counter('net_sent_total', 'Totally sent in bytes',
+        for k, per_thread_mertics in pairs(box_stat_net_per_thread) do
+            local index = tostring(k)
+
+            collectors_list.net_per_thread_sent_total =
+                utils.set_counter('net_per_thread_sent_total', 'Totally sent in bytes',
                     per_thread_mertics.SENT.total, {thread = index})
 
-            collectors_list.net_sent_rps =
-                utils.set_gauge('net_sent_rps', 'Sending RPS',
-                    per_thread_mertics.SENT.rps, {thread = index})
-
-            collectors_list.net_received_total =
-                utils.set_counter('net_received_total', 'Totally received in bytes',
+            collectors_list.net_per_thread_received_total =
+                utils.set_counter('net_per_thread_received_total', 'Totally received in bytes',
                     per_thread_mertics.RECEIVED.total, {thread = index})
 
-            collectors_list.net_received_rps =
-                utils.set_gauge('net_received_rps', 'Receive RPS',
-                    per_thread_mertics.RECEIVED.rps, {thread = index})
-
             if per_thread_mertics.CONNECTIONS ~= nil and type(per_thread_mertics.CONNECTIONS) ~= 'number' then
-                collectors_list.net_connections_rps =
-                    utils.set_gauge('net_connections_rps', 'Connection RPS',
-                        per_thread_mertics.CONNECTIONS.rps, {thread = index})
-
-                collectors_list.net_connections_total =
-                    utils.set_counter('net_connections_total', 'Connections total amount',
+                collectors_list.net_per_thread_connections_total =
+                    utils.set_counter('net_per_thread_connections_total', 'Connections total amount',
                         per_thread_mertics.CONNECTIONS.total, {thread = index})
 
-                collectors_list.net_connections_current =
-                    utils.set_gauge('net_connections_current', 'Current connections amount',
+                collectors_list.net_per_thread_connections_current =
+                    utils.set_gauge('net_per_thread_connections_current', 'Current connections amount',
                         per_thread_mertics.CONNECTIONS.current, {thread = index})
 
             elseif per_thread_mertics.CONNECTIONS ~= nil then
-                collectors_list.net_connections_current =
-                    utils.set_gauge('net_connections_current', 'Current connections amount',
+                collectors_list.net_per_thread_connections_current =
+                    utils.set_gauge('net_per_thread_connections_current', 'Current connections amount',
                         per_thread_mertics.CONNECTIONS, {thread = index})
             end
 
             if per_thread_mertics.REQUESTS ~= nil then
-                collectors_list.net_requests_rps =
-                    utils.set_gauge('net_requests_rps', 'Requests RPS',
-                        per_thread_mertics.REQUESTS.rps, {thread = index})
-
-                collectors_list.net_requests_total =
-                    utils.set_counter('net_requests_total', 'Requests total amount',
+                collectors_list.net_per_thread_requests_total =
+                    utils.set_counter('net_per_thread_requests_total', 'Requests total amount',
                         per_thread_mertics.REQUESTS.total, {thread = index})
 
-                collectors_list.net_requests_current =
-                    utils.set_gauge('net_requests_current', 'Pending requests',
+                collectors_list.net_per_thread_requests_current =
+                    utils.set_gauge('net_per_thread_requests_current', 'Pending requests',
                         per_thread_mertics.REQUESTS.current, {thread = index})
             end
 
             if per_thread_mertics.REQUESTS_IN_PROGRESS ~= nil then
-                collectors_list.net_requests_in_progress_total =
-                    utils.set_counter('net_requests_in_progress_total', 'Requests in progress total amount',
+                collectors_list.net_per_thread_requests_in_progress_total =
+                    utils.set_counter('net_per_thread_requests_in_progress_total', 'Requests in progress total amount',
                         per_thread_mertics.REQUESTS_IN_PROGRESS.total, {thread = index})
 
-                collectors_list.net_requests_in_progress_current =
-                    utils.set_gauge('net_requests_in_progress_current',
+                collectors_list.net_per_thread_requests_in_progress_current =
+                    utils.set_gauge('net_per_thread_requests_in_progress_current',
                         'Count of requests currently being processed in the tx thread',
                         per_thread_mertics.REQUESTS_IN_PROGRESS.current, {thread = index})
             end
 
             if per_thread_mertics.REQUESTS_IN_STREAM_QUEUE ~= nil then
-                collectors_list.net_requests_in_stream_queue_total =
-                    utils.set_counter('net_requests_in_stream_queue_total',
+                collectors_list.net_per_thread_requests_in_stream_queue_total =
+                    utils.set_counter('net_per_thread_requests_in_stream_queue_total',
                         'Total count of requests, which was placed in queues of streams',
                         per_thread_mertics.REQUESTS_IN_STREAM_QUEUE.total, {thread = index})
 
-                collectors_list.net_requests_in_stream_queue_current =
-                    utils.set_gauge('net_requests_in_stream_queue_current',
+                collectors_list.net_per_thread_requests_in_stream_queue_current =
+                    utils.set_gauge('net_per_thread_requests_in_stream_queue_current',
                         'Count of requests currently waiting in queues of streams',
                         per_thread_mertics.REQUESTS_IN_STREAM_QUEUE.current, {thread = index})
             end
