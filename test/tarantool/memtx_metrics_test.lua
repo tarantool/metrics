7b155d507e875194c0e1c479b08136753458d9e9
Andrei Sidorov <andrei.sidorov@vk.team>
2022-08-18 22:59:20 +0300

    fix 7

--- test/tarantool/memtx_metrics_test.lua
+++ test/tarantool/memtx_metrics_test.lua
@@ -15,47 +15,45 @@ g.after_each(function()
 end)
 
 g.test_memtx = function()
+    t.skip_if(utils.is_version_less(_TARANTOOL, '2.10.0'),
+        'Tarantool version is must be v2.10.0 or greater')
+
+    metrics.enable_default_metrics()
+    memtx.update()
+    local default_metrics = metrics.collect()
     local log = require('log')
 
-    if utils.is_version_less(_TARANTOOL, '2.10.0') then
-        log.info("test_memtx skipped")
-    else
-        metrics.enable_default_metrics()
-        memtx.update()
-        local default_metrics = metrics.collect()
+    local metrics_list1 = {
+        'tnt_memtx_tnx_statements',
+        'tnt_memtx_tnx_user',
+        'tnt_memtx_tnx_system',
+        'tnt_memtx_mvcc_trackers',
+        'tnt_memtx_mvcc_conflicts',
+    }
 
-        local metrics_list1 = {
-            'tnt_memtx_tnx_statements',
-            'tnt_memtx_tnx_user',
-            'tnt_memtx_tnx_system',
-            'tnt_memtx_mvcc_trackers',
-            'tnt_memtx_mvcc_conflicts',
-        }
+    for _, item in ipairs(metrics_list1) do
+        log.info('checking metric: ' .. item)
+        local metric = utils.find_metric(item, default_metrics)
+        t.assert(metric)
+        t.assert_items_equals(metric[1].label_pairs, {kind = "average"})
+        t.assert_items_equals(metric[2].label_pairs, {kind = "total"})
+        t.assert_items_equals(metric[3].label_pairs, {kind = "max"})
+    end
 
-        for _, item in ipairs(metrics_list1) do
-            log.info('checking metric: ' .. item)
-            local metric = utils.find_metric(item, default_metrics)
-            t.assert(metric)
-            t.assert_items_equals(metric[1].label_pairs, {kind = "average"})
-            t.assert_items_equals(metric[2].label_pairs, {kind = "total"})
-            t.assert_items_equals(metric[3].label_pairs, {kind = "max"})
-        end
+    local metrics_list2 = {
+        'tnt_memtx_mvcc_tuples_tracking_stories',
+        'tnt_memtx_mvcc_tuples_tracking_retained',
+        'tnt_memtx_mvcc_tuples_used_stories',
+        'tnt_memtx_mvcc_tuples_used_retained',
+        'tnt_memtx_mvcc_tuples_read_view_stories',
+        'tnt_memtx_mvcc_tuples_read_view_retained',
+    }
 
-        local metrics_list2 = {
-            'tnt_memtx_mvcc_tuples_tracking_stories',
-            'tnt_memtx_mvcc_tuples_tracking_retained',
-            'tnt_memtx_mvcc_tuples_used_stories',
-            'tnt_memtx_mvcc_tuples_used_retained',
-            'tnt_memtx_mvcc_tuples_read_view_stories',
-            'tnt_memtx_mvcc_tuples_read_view_retained',
-        }
-
-        for _, item in ipairs(metrics_list2) do
-            log.info('checking metric: ' .. item)
-            local metric = utils.find_metric(item, default_metrics)
-            t.assert(metric)
-            t.assert_items_equals(metric[1].label_pairs, {kind = "total"})
-            t.assert_items_equals(metric[2].label_pairs, {kind = "count"})
-        end
+    for _, item in ipairs(metrics_list2) do
+        log.info('checking metric: ' .. item)
+        local metric = utils.find_metric(item, default_metrics)
+        t.assert(metric)
+        t.assert_items_equals(metric[1].label_pairs, {kind = "total"})
+        t.assert_items_equals(metric[2].label_pairs, {kind = "count"})
     end
 end
