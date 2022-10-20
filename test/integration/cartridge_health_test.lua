ba48645430d32ae6bdab479328c8f273a6604532
Eugene Paniot <e.paniot@gmail.com>
2022-10-20 17:12:54 +0100

    Add health tests for membership state

--- test/integration/cartridge_health_test.lua
+++ test/integration/cartridge_health_test.lua
@@ -1,96 +1,78 @@
-local fio = require('fio')
-local t = require('luatest')
+local fio = require("fio")
+local t = require("luatest")
 local g = t.group()
 
-local helpers = require('test.helper')
+local helpers = require("test.helper")
 
-g.before_all(function()
-    t.skip_if(type(helpers) ~= 'table', 'Skip cartridge test')
-    g.cluster = helpers.init_cluster()
-end)
+g.before_all(
+    function()
+        t.skip_if(type(helpers) ~= "table", "Skip cartridge test")
+        g.cluster = helpers.init_cluster()
+    end
+)
 
-g.after_all(function()
-    g.cluster:stop()
-    fio.rmtree(g.cluster.datadir)
-end)
+g.after_all(
+    function()
+        g.cluster:stop()
+        fio.rmtree(g.cluster.datadir)
+    end
+)
 
 g.test_cartridge_health_handler = function()
-    helpers.skip_cartridge_version_less('2.0.2')
+    helpers.skip_cartridge_version_less("2.0.2")
     helpers.upload_default_metrics_config(g.cluster)
-    local main_server = g.cluster:server('main')
-    local resp = main_server:http_request('get', '/health', {raise = false})
+    local main_server = g.cluster:server("main")
+    local resp = main_server:http_request("get", "/health", {raise = false})
     t.assert_equals(resp.status, 200)
 end
 
 g.test_cartridge_health_fail_handler = function()
-    helpers.skip_cartridge_version_less('2.0.2')
+    helpers.skip_cartridge_version_less("2.0.2")
     helpers.upload_default_metrics_config(g.cluster)
-    local main_server = g.cluster:server('main')
-    main_server.net_box:eval([[
+    local main_server = g.cluster:server("main")
+    main_server.net_box:eval(
+        [[
         _G.old_info = box.info
         box.info = {
             status = 'orphan',
         }
-    ]])
-    local resp = main_server:http_request('get', '/health', {raise = false})
+    ]]
+    )
+    local resp = main_server:http_request("get", "/health", {raise = false})
     t.assert_equals(resp.status, 500)
     main_server.net_box:eval([[
         box.info = _G.old_info
     ]])
 end
 
-g.test_cartridge_health_handler_state_1 = function()
-    helpers.skip_cartridge_version_less('2.0.2')
+g.test_cartridge_health_handler_member_state_configured_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
     helpers.upload_default_metrics_config(g.cluster)
-    local main_server = g.cluster:server('main')
-    main_server.net_box:eval([[
-        _G.old_info = box.info
-        box.info = {
-            status = 'alive',
-            state = 'RolesConfigured'
-        }
-    ]])
-    local resp = main_server:http_request('get', '/health', {raise = false})
+    local main_server = g.cluster:server("main")
+    main_server.net_box:eval(
+        [[
+        myself = require('membership').myself()
+
+        myself.payload.state='ConfiguringRoles'
+        myself.payload.state_prev='RolesConfigured'
+    ]]
+    )
+    local resp = main_server:http_request("get", "/health", {raise = false})
     t.assert_equals(resp.status, 200)
-    main_server.net_box:eval([[
-        box.info = _G.old_info
-    ]])
 end
 
-g.test_cartridge_health_handler_state_2 = function()
-    helpers.skip_cartridge_version_less('2.0.2')
+g.test_cartridge_health_handler_member_state_boxconfigured_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
     helpers.upload_default_metrics_config(g.cluster)
-    local main_server = g.cluster:server('main')
-    main_server.net_box:eval([[
-        _G.old_info = box.info
-        box.info = {
-            status = 'alive',
-            state = 'ConfiguringRoles',
-            state_prev = 'RolesConfigured'
-        }
-    ]])
-    local resp = main_server:http_request('get', '/health', {raise = false})
-    t.assert_equals(resp.status, 200)
-    main_server.net_box:eval([[
-        box.info = _G.old_info
-    ]])
-end
+    local main_server = g.cluster:server("main")
+    main_server.net_box:eval(
+        [[
+        myself = require('membership').myself()
 
-g.test_cartridge_health_fail_handler_state_1 = function()
-    helpers.skip_cartridge_version_less('2.0.2')
-    helpers.upload_default_metrics_config(g.cluster)
-    local main_server = g.cluster:server('main')
-    main_server.net_box:eval([[
-        _G.old_info = box.info
-        box.info = {
-            status = 'alive',
-            state = 'ConfiguringRoles',
-            state_prev = 'BoxConfigured'
-        }
-    ]])
-    local resp = main_server:http_request('get', '/health', {raise = false})
+        myself.payload.state='ConfiguringRoles'
+        myself.payload.state_prev='BoxConfigured'
+    ]]
+    )
+    local resp = main_server:http_request("get", "/health", {raise = false})
     t.assert_equals(resp.status, 500)
-    main_server.net_box:eval([[
-        box.info = _G.old_info
-    ]])
 end
