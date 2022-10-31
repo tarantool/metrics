ff958d1d0cddbbb78dc3ee9ecb979790acd4d8c3
Eugene Paniot <e.paniot@gmail.com>
2022-10-31 14:22:07 +0000

    Use the same logic in is_healthy as member_is_healthy from cartridge (#411)

--- test/integration/cartridge_health_test.lua
+++ test/integration/cartridge_health_test.lua
@@ -38,3 +38,95 @@ g.test_cartridge_health_fail_handler = function()
         box.info = _G.old_info
     ]])
 end
+
+g.test_cartridge_health_handler_member_alive_state_configured_to_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
+    helpers.upload_default_metrics_config(g.cluster)
+    local main_server = g.cluster:server("main")
+
+    main_server:exec(function()
+        local membership = package.loaded['membership']
+        
+        membership.myself = function()
+            return {
+                status = 'alive',
+                payload = {
+                    state='ConfiguringRoles',
+                    state_prev='RolesConfigured',
+                }
+            }
+        end
+    end)
+
+    local resp = main_server:http_request("get", "/health", {raise = false})
+    t.assert_equals(resp.status, 200)
+end
+
+g.test_cartridge_health_handler_member_suspect_state_configured_to_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
+    helpers.upload_default_metrics_config(g.cluster)
+    local main_server = g.cluster:server("main")
+
+    main_server:exec(function()
+        local membership = package.loaded['membership']
+        
+        membership.myself = function()
+            return {
+                status = 'suspect',
+                payload = {
+                    state='ConfiguringRoles',
+                    state_prev='RolesConfigured',
+                }
+            }
+        end
+    end)
+
+    local resp = main_server:http_request("get", "/health", {raise = false})
+    t.assert_equals(resp.status, 200)
+end
+
+g.test_cartridge_health_handler_member_alive_state_boxconfigured_to_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
+    helpers.upload_default_metrics_config(g.cluster)
+    local main_server = g.cluster:server("main")
+
+    main_server:exec(function()
+        local membership = package.loaded['membership']
+
+        membership.myself = function()
+            return {
+                status = 'alive',
+                payload = {
+                    state='ConfiguringRoles',
+                    state_prev='BoxConfigured',
+                }
+            }
+        end
+    end)
+
+    local resp = main_server:http_request("get", "/health", {raise = false})
+    t.assert_equals(resp.status, 500)
+end
+
+g.test_cartridge_health_handler_member_suspect_state_boxconfigured_to_configuring = function()
+    helpers.skip_cartridge_version_less("2.0.2")
+    helpers.upload_default_metrics_config(g.cluster)
+    local main_server = g.cluster:server("main")
+
+    main_server:exec(function()
+        local membership = package.loaded['membership']
+
+        membership.myself = function()
+            return {
+                status = 'suspect',
+                payload = {
+                    state='ConfiguringRoles',
+                    state_prev='BoxConfigured',
+                }
+            }
+        end
+    end)
+
+    local resp = main_server:http_request("get", "/health", {raise = false})
+    t.assert_equals(resp.status, 500)
+end
