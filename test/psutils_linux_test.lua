d431ebb020e33f05442cd33fd1b5bc42119ba4cf
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-09 17:08:12 +0300

    test: rework psutils test files mock
    Use debug utils to build the full path to mock files. Overwrite fio
    handles instead of fio package since it is impossible to overwrite fio
    package used in psutils code after tarantool startup for embedded code.
    
    Part of tarantool/tarantool#7725

--- test/psutils_linux_test.lua
+++ test/psutils_linux_test.lua
@@ -4,32 +4,26 @@ local t = require('luatest')
 local g = t.group('psutils_linux')
 
 local fio = require('fio')
+local fio_impl = table.deepcopy(fio)
 
-local payload_dir = os.getenv('PWD') .. '/test/psutils_linux_test_payload'
-local stat_file_path = payload_dir .. '/proc_stat'
-local task_dir_path = payload_dir .. '/proc_self_task'
+local psutils_mock = require('test.psutils_linux_test_payload')
 
-local psutils_linux
+local psutils_linux = require('metrics.psutils.psutils_linux')
+
 g.before_all(function()
     t.skip_if(jit.os ~= 'Linux', 'Linux is the only supported platform')
 
-    local task_files = {
-        ['/proc/stat'] = stat_file_path,
-        ['/proc/self/task/1/stat'] = task_dir_path .. '/1/stat',
-        ['/proc/self/task/12/stat'] = task_dir_path .. '/12/stat',
-        ['/proc/self/task/13/stat'] = task_dir_path .. '/13/stat',
-        ['/proc/self/task/14/stat'] = task_dir_path .. '/14/stat',
-    }
-    package.loaded['fio'] = {
-        open = function(path, mode)
-            return fio.open(task_files[path], mode)
-        end,
-        listdir = function(_)
-            return fio.listdir(task_dir_path)
-        end,
-    }
+    fio.open = function(path, mode)
+        return fio_impl.open(psutils_mock.files[path], mode)
+    end
+    fio.listdir = function(_)
+        return fio_impl.listdir(psutils_mock.task_dir_path)
+    end
+end)
 
-    psutils_linux = require('metrics.psutils.psutils_linux')
+g.after_all(function()
+    fio.open = fio_impl.open
+    fio.listdir = fio_impl.listdir
 end)
 
 g.test_get_cpu_time = function()
