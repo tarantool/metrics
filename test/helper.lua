c0deb20b3cbd97829d8c0d44941ae2d2a079e304
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-09 17:35:51 +0300

    test: remove built-in metrics
    After package embedding, `require('metrics')` will return in-built
    module. To run tests with repository code on integration tests, we must
    first remove built-in package.
    
    Part of tarantool/tarantool#7725

--- test/helper.lua
+++ test/helper.lua
@@ -1,5 +1,11 @@
 require('strict').on()
 
+-- Clean up built-in metrics to run tests with repository module.
+local rock_utils = require('test.rock_utils')
+rock_utils.remove_builtin('metrics')
+rock_utils.assert_nonbuiltin('metrics')
+
+
 local fio = require('fio')
 local utils = require('test.utils')
 local t = require('luatest')
