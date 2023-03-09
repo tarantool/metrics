68c1d51a350022a5bff56fd26eb2efbd01f83bb5
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-09 17:35:51 +0300

    test: remove built-in metrics
    After package embedding, `require('metrics')` will return in-built
    module. To run tests with repository code on integration tests, we must
    first remove built-in package.
    
    Part of tarantool/tarantool#7725

---
+++ test/rock_utils.lua
@@ -0,0 +1,85 @@
+local loaders_ok, loaders = pcall(require, 'internal.loaders')
+
+local function traverse_rock(func, name)
+    func(name, package.loaded[name])
+
+    for subpkg_name, subpkg in pairs(package.loaded) do
+        if subpkg_name:startswith(name .. '.') then
+            func(subpkg_name, subpkg)
+        end
+    end
+end
+
+-- Metrics may have table cross-references.
+local MAX_DEPTH = 8
+
+-- Package functions contain useful debug info.
+local function traverse_pkg_func(func, name, pkg, max_depth)
+    max_depth = max_depth or MAX_DEPTH
+    if max_depth <= 0 then
+        return
+    end
+
+    if type(pkg) == 'function' then
+        func(name, pkg)
+        return
+    end
+
+    if type(pkg) ~= 'table' then
+        return
+    end
+
+    for _, v in pairs(pkg) do
+        traverse_pkg_func(func, name, v, max_depth - 1)
+    end
+
+    local mt = getmetatable(pkg)
+    if mt ~= nil and mt.__call ~= nil then
+        func(name, mt.__call)
+    end
+end
+
+local function remove_builtin_pkg(name, _)
+    package.loaded[name] = nil
+    if loaders_ok then
+        loaders.builtin[name] = nil
+    end
+end
+
+local function assert_nonbuiltin_func(pkg_name, func)
+    local source = debug.getinfo(func).source
+    assert(source:match('^@builtin') == nil,
+           ("package %s is built-in, cleanup failed"):format(pkg_name))
+end
+
+local function assert_builtin_func(pkg_name, func)
+    local source = debug.getinfo(func).source
+    assert(source:match('^@builtin') ~= nil,
+           ("package %s is external, built-in expected"):format(pkg_name))
+end
+
+local function assert_nonbuiltin_pkg(name, pkg)
+    traverse_pkg_func(assert_nonbuiltin_func, name, pkg)
+end
+
+local function assert_builtin_pkg(name, pkg)
+    traverse_pkg_func(assert_builtin_func, name, pkg)
+end
+
+local function remove_builtin_rock(name)
+    traverse_rock(remove_builtin_pkg, name)
+end
+
+local function assert_nonbuiltin_rock(name)
+    traverse_rock(assert_nonbuiltin_pkg, name)
+end
+
+local function assert_builtin_rock(name)
+    traverse_rock(assert_builtin_pkg, name)
+end
+
+return {
+    assert_builtin = assert_builtin_rock,
+    assert_nonbuiltin = assert_nonbuiltin_rock,
+    remove_builtin = remove_builtin_rock,
+}
