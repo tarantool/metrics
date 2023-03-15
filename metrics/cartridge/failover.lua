b53b9336c60a381c5d28eb14b3465de714b1ce21
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-15 09:26:36 +0300

    metrics: check external module on update
    Part of tarantool/tarantool#7725

--- metrics/cartridge/failover.lua
+++ metrics/cartridge/failover.lua
@@ -1,17 +1,17 @@
-local is_cartridge = pcall(require, 'cartridge')
-
-if not is_cartridge then
-    return {
-        update = function() end,
-        list = {},
-    }
-end
-
 local utils = require('metrics.utils')
 local collectors_list = {}
-local vars = require('cartridge.vars').new('cartridge.failover')
+local vars = nil
 
 local function update()
+    if vars == nil then
+        local is_cartridge = pcall(require, 'cartridge')
+        if not is_cartridge then
+            return
+        end
+
+        vars = require('cartridge.vars').new('cartridge.failover')
+    end
+
     local trigger_cnt = vars.failover_trigger_cnt
     if trigger_cnt ~= nil then
         collectors_list.trigger_cnt =
--- metrics/tarantool/clock.lua
+++ metrics/tarantool/clock.lua
@@ -1,14 +1,13 @@
 local utils = require('metrics.utils')
-local ok, membership = pcall(require, 'membership')
-
-if not ok then
-    return { update = function() end }
-end
 
 local collectors_list = {}
 
 -- from https://github.com/tarantool/cartridge/blob/cc607f5a6508449608f3953a3f93669e8c8c4ab0/cartridge/issues.lua#L375
 local function update_clock_metrics()
+    local ok, membership = pcall(require, 'membership')
+    if not ok then
+        return
+    end
 
     local min_delta = 0
     local max_delta = 0
