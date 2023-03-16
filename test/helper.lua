666f7b29da6297eaf67157fddf748fb9b417eb94
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-16 18:02:03 +0300

    test: require package before assert
    For now, checker is useless since it deals with package.loaded and there
    are no metrics rock data after remove_builtin.
    
    Part of tarantool/tarantool#7725

--- test/helper.lua
+++ test/helper.lua
@@ -3,6 +3,7 @@ require('strict').on()
 -- Clean up built-in metrics to run tests with repository module.
 local rock_utils = require('test.rock_utils')
 rock_utils.remove_builtin('metrics')
+require('metrics')
 rock_utils.assert_nonbuiltin('metrics')
 
 
