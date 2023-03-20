4203b3b0f0e7228dddb7eba42fc359abfc3b202e
Georgy Moiseev <georgy.moiseev@corp.mail.ru>
2023-03-20 18:18:32 +0300

    test: rework internal asserts

--- test/helper.lua
+++ test/helper.lua
@@ -3,7 +3,6 @@ require('strict').on()
 -- Clean up built-in metrics to run tests with repository module.
 local rock_utils = require('test.rock_utils')
 rock_utils.remove_builtin('metrics')
-require('metrics')
 rock_utils.assert_nonbuiltin('metrics')
 
 
